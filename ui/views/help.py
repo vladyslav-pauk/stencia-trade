3ce8ecbd616b11779b408f2ab1fd3c0708ae7c06
paukvlad <82984225+paukvlad@users.noreply.github.com>
2025-02-18 04:05:38 -0800

    version 1

--- ui/views/help.py
+++ ui/views/help.py
@@ -1,7 +1,6 @@
 def help_tab(st):
     """Displays help text based on the selected tab in the sidebar."""
 
-    # Help text for each tab
     text = {
         "About": "Stencia-Trade is a Streamlit application that allows users to analyze and visualize stock data, backtest trading strategies, and search for trading opportunities.",
         "Chart": "This is the chart tab.",
@@ -10,20 +9,18 @@ def help_tab(st):
         "Portfolio": "This is the portfolio tab."
     }
 
-    # Ensure session state has a valid tab
     if "selected_page" not in st.session_state:
-        st.session_state.selected_page = "About"  # Default to About
+        st.session_state.selected_page = "About"
 
     with st.sidebar:
         st.header("Help")
         selected_tab = st.radio("Contents", list(text.keys()),
                                 index=list(text.keys()).index(st.session_state.selected_page))
 
-        # Only update the session state if the selection changes
+
         if selected_tab != st.session_state.selected_page:
             st.session_state.selected_page = selected_tab
-            st.rerun()  # Force a refresh to apply changes instantly
+            st.rerun()
 
-    # Display the selected tab's content
     st.header(st.session_state.selected_page)
     st.write(text.get(st.session_state.selected_page))
